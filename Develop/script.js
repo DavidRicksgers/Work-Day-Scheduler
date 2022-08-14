92662e13033262f6a82ae4ca47cf9ba5acb97f27
David. Ricksgers <david@gmail.com>
2022-08-14 16:58:22 -0700

    added var for todays date, added event listener for save buttons, save text in local storage when save button is clicked, set recall from local storage for each hour block.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,29 @@
 // Display today's day and date
+let todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
 //hour block functionality
-// Save text in local storage
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        let text = $(this).siblings(".description").val();
+        let time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+})
 // check time for background indicators 
 //add and remove the classes for background indicators
 // Get item from local storage if any
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
