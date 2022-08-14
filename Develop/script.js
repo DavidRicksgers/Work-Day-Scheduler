d52e451446478d210bd95b61c9934ef00ec3dc80
David. Ricksgers <david@gmail.com>
2022-08-14 16:44:10 -0700

    outlined different sections of javascript code I will need to implement

---
+++ Develop/script.js
@@ -0,0 +1,6 @@
+// Display today's day and date
+//hour block functionality
+// Save text in local storage
+// check time for background indicators 
+//add and remove the classes for background indicators
+// Get item from local storage if any
